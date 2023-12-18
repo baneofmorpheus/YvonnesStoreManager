d12534fe3c2f2e4075f5b611debd8573b3fcab1e
Leon Chux <epicgenii18@gmail.com>
2023-12-17 21:19:17 -0400

    general views and deps upate

--- navigation/RootStackNavigation.tsx
+++ navigation/RootStackNavigation.tsx
@@ -3,19 +3,24 @@ import HomeScreen from '../screens/HomeScreen';
 import ProductsScreen from '../screens/ProductsScreen';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import { Ionicons, Entypo, MaterialIcons,FontAwesome,AntDesign,Feather } from "@expo/vector-icons"
 export type RootStackNavigationType = {
     Products: { name: string };
     Home: { name: string };
+    Settings: { name: string };
+    Sales: { name: string };
 };
 const Stack = createBottomTabNavigator<RootStackNavigationType>();
 
 const RootStackNavigation = () => {
+    const iconSize = 24;
+    const iconColor = 'black';
     return (
         <Stack.Navigator initialRouteName="Home"
             screenOptions={{
-                tabBarStyle: { paddingBottom: 10 },
+                tabBarStyle: { marginBottom: 3,padding:10 },
                 tabBarLabelStyle: { fontSize: 12 },
-                headerShown:false
+                headerShown: false
             }}
 
 
@@ -25,16 +30,36 @@ const RootStackNavigation = () => {
                 component={HomeScreen}
                 options={{
                     title: 'Welcome', tabBarLabel: 'Home',
-                    tabBarIcon: () => null,
+                    tabBarIcon: () => <AntDesign name='home' size={iconSize} color={iconColor} />,
                 }
                 }
             />
+            < Stack.Screen name="Sales"
+
+                options={{
+                    title: 'Sales', tabBarLabel: 'Sales',
+
+                    tabBarIcon: () => <MaterialIcons name="attach-money" size={iconSize} color={iconColor} />,
+                }
+                }
+
+                component={ProductsScreen} />
             < Stack.Screen name="Products"
 
                 options={{
                     title: 'Products', tabBarLabel: 'Products',
 
-                    tabBarIcon: () => null,
+                    tabBarIcon: () => <Feather name="box" size={iconSize} color={iconColor} />,
+                }
+                }
+
+                component={ProductsScreen} />
+            < Stack.Screen name="Settings"
+
+                options={{
+                    title: 'Settings', tabBarLabel: 'Settings',
+
+                    tabBarIcon: () => <Feather name='settings' size={iconSize} color={iconColor} />,
                 }
                 }
 
